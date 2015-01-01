675b9697c88994920e1c08453cae332694b0f092
Martyn Mayfield <martyn.mayfield@gmail.com>
2015-01-01 22:35:01 +0800

    modified the variables

--- old/WAGWAMinsert.php
+++ old/WAGWAMinsert.php
@@ -13,17 +13,19 @@ $complete=mysql_real_escape_string($_POST['complete']);
 $evidence=mysql_real_escape_string($_POST['evidence']);
 $improve=mysql_real_escape_string($_POST['improve']);
 $modify=mysql_real_escape_string($_POST['modify']);
-//question on the following concerning coding
-//following only appears at weeks 3,6,9.
-$emotion=mysql_real_escape_string($_POST['emotion']);
-//following only appears in week 12
-$met=mysql_real_escape_string($_POST['met']);
+
 $what=mysql_real_escape_string($_POST['what']);
 $when=mysql_real_escape_string($_POST['when']);
 $location=mysql_real_escape_string($_POST['location']);
 //$times = $perday*perweek
 $times=mysql_real_escape_string($_POST['times']);
-$Comment=mysql_real_escape_string($_POST['Comment']);
+$comment=mysql_real_escape_string($_POST['comment']);
+
+//question on the following concerning coding
+//following only appears at weeks 3,6,9.
+$emotion=mysql_real_escape_string($_POST['emotion']);
+//following only appears in week 12
+$met=mysql_real_escape_string($_POST['met']);
 
 //mysql_select_db("UMLC_data") or die(mysql_error()); IS THIS NEEDED SINCE ABOVE HAS ALREADY INCLUDED UMLCdatalogin.php ?
 //not needed because when we created the 'mysqli' object in UMLCdatalogin.php, we included the name of the database
