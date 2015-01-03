0f14221ba9b98049010b4e6619c70df00db97d44
ZealJared <zeal.j.mayfield@gmail.com>
2015-01-03 02:20:01 -0800

    visual enhancements

--- allteachertasks.php
+++ allteachertasks.php
@@ -1,15 +1,29 @@
-<pre>
+<style>
+    span {
+        display: inline-block;
+        padding: 5px;
+        width: 300px;
+    }
+    div:nth-child(odd) {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+</style>
+
 <?php
 
 require_once 'UMLCdatalogin.php';
 
-$sql = "SELECT * FROM tasks JOIN teacher ON(tasks.teacher_id = teacher.id) ORDER BY teacher.name, tasks.level ASC";
+$sql = "SELECT teacher.name, tasks.task, tasks.level FROM tasks JOIN teacher ON(tasks.teacher_id = teacher.id) ORDER BY teacher.name, tasks.level ASC";
 
 $tasks = $DB->query($sql);
 
 $task_rows = $tasks->fetch_all(MYSQLI_ASSOC);
 
-echo var_dump($task_rows);
+foreach($task_rows as $task){
+    echo "<div>";
+    foreach($task as $field => $value){
+        echo "<span>" . $field . ": " . $value . "</span>";
+    }
+    echo "</div>";
+}
 
-?>
-</pre>
